9912d23a9b38c266ea8c7ec59d951522778a8045
akishinevsky <akishinevsky@gsngames.com>
2017-02-22 16:42:59 -0800

    Revert "adding demo files"
    This reverts commit 8bd276cef184ab9613f77fb76420c9a3ae198b55.

--- alextestsite/polls/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^index_check/', views.index_check, name='index_check'),
-]
